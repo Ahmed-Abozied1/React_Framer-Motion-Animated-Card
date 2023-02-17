e4796d685491e392fa3ba3121beb659cf6b3db88
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-02-17 21:46:32 +0200

    add List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -9,6 +9,15 @@ const List = ({ data }) => {
         <div className={styles.list_Image_container}>
           <img src={imageUrl} className={styles.list_Image} alt="Image1" />
         </div>
+        <div className={styles.list_details}>
+        <div className={styles.list_type}>Sale</div>
+          <div className={styles.list_row}>
+            <span className={styles.list_price}>{price}</span>
+          </div>
+          <div className={styles.list_row}>
+            <span className={styles.list_address}>{address}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
