8904ccc32ba776772e19d82439d693b24c85f4af
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-02-17 22:39:10 +0200

    add List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -10,13 +10,40 @@ const List = ({ data }) => {
           <img src={imageUrl} className={styles.list_Image} alt="Image1" />
         </div>
         <div className={styles.list_details}>
-        <div className={styles.list_type}>Sale</div>
+          <div className={styles.list_type}>Sale</div>
           <div className={styles.list_row}>
             <span className={styles.list_price}>{price}</span>
           </div>
           <div className={styles.list_row}>
             <span className={styles.list_address}>{address}</span>
           </div>
+         <div className={styles.feature}>
+         <div className={styles.list_row}>
+            <div className={styles.bed_icon}>
+              <i
+                className="fa-sharp fa-solid fa-bed"
+                style={{ fontSize: 24, color:"gray" }}
+              ></i>
+            </div>
+            <span className={styles.list_numbed}>{numBedroom}</span>
+          </div>
+          <div className={styles.list_row}>
+            <div className={styles.bed_icon}>
+            <i class="fa-sharp fa-solid fa-shower" style={{ fontSize: 24, color:"gray" }}></i>
+            </div>
+            <span className={styles.list_numbed}>{numWashrooms}</span>
+          </div>
+          <div className={styles.list_row}>
+            <div className={styles.bed_icon}>
+              <i
+                className="fa-solid fa-ruler"
+                style={{ fontSize: 24, color:"gray" }}
+              ></i>
+            </div>
+            <span className={styles.list_numbed}>{livingSpace}</span>
+          </div>
+         </div>
+          
         </div>
       </div>
     </div>
--- src/constants/data.js
+++ src/constants/data.js
@@ -7,8 +7,8 @@ export const properties = [
       address: "632 Fake Address",
       description:
         "Beautiful house! Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
-      numBedroom: 4,
-      numWashrooms: 3,
+      numBedroom: 2,
+      numWashrooms: 2,
       livingSpace: "3,000 sq.ft.",
     },
     {
@@ -19,8 +19,8 @@ export const properties = [
       address: "1204 Fake Address",
       description:
         "Beautiful house! Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
-      numBedroom: 4,
-      numWashrooms: 3,
+      numBedroom: 3,
+      numWashrooms: 2,
       livingSpace: "2,410 sq.ft.",
     },
     {
@@ -30,8 +30,8 @@ export const properties = [
       address: "7177 Fake Address",
       description:
         "Beautiful house! Offers you 4 bedrooms, 3 full bathrooms with separate showers. Lots of windows and natural light. The finished basement includes a bedroom, a large family room and a full bathroom. Great location! Close to daycares, schools and accessible by public transport. 5 minutes by car from the downtown area. Quick access to highway and subway. On the second floor: The master bedroom is spacious. It has a balcony, a large walk-in closet, and its own en-suite bathroom. The other three bedrooms are of good size and each has its own walk-in closet. The basement is fully finished and offers a family room, as well as a game room. The basement bedroom is convenient with a full bathroom.",
-      numBedroom: 4,
-      numWashrooms: 3,
+      numBedroom: 6,
+      numWashrooms: 1,
       livingSpace: "4,450 sq.ft.",
     },
   ];
